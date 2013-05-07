a3223ecf8158ee310f1ca02df82083585438bed2
Bonnie Coit <bcoit@csm-pm-38123-15.local>
2013-05-07 15:59:44 -0700

    midterm

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -1,3 +1,11 @@
+def pointField(x,y,q,Xq,Yq):
+    """Takes the arrays (x,y), charge q, and position (Xq,Yq) as arguments and returns a tuple of electric field components (Ex,Ey)"""
+    k=8.987551787e9 #units of k: N*m^2/C^2
+    Ex = k*q*(x-Xq)/(((x-Xq)**2+(y-Yq)**2)**(1/2.))
+    Ey = k*q*(y-Yq)/(((x-Xq)**2+(y-Yq)**2)**(1/2.))
+    ExEy = [Ex, Ey]
+    return ExEy
+
 def pointPotential(x,y,q,posx,posy):
     """This function will take an x-y coord, charge, and position of the charge and return the potential at that x-y point"""
     k=8.987551787e9 #units of k: N*m^2/C^2
